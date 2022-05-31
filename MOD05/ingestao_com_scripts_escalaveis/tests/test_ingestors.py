cae0dc4852003df176cac1b2c827ef5fd8f4b2b6
tiagovianez <tiagovianez@live.com>
2022-05-30 23:47:08 -0300

    update test_ingestors

--- MOD05/ingestao_com_scripts_escalaveis/tests/test_ingestors.py
+++ MOD05/ingestao_com_scripts_escalaveis/tests/test_ingestors.py
@@ -1,18 +1,18 @@
-# import datetime
-# from unittest.mock import patch
-# from ingestors import DataIngestor
-# from writers import DataWriter
+import datetime
+from unittest.mock import patch
+from ingestors import DataIngestor
+from writers import DataWriter
 
 
-# # Subscrevendo um método de um determinada classe
-# # Nesse caso estou informando que essa classe não tem mais nenhum objeto definido 
-# @patch("ingestors.DataIngestor.__abstractmethod__", set())
-# class TestIngestors:
-#     def test_checkpoint_filename(self):
-#         actual = DataIngestor(
-#             writer=DataWriter,
-#             coins=["TEST", "HOW"],
-#             default_start_date=datetime.date(2021, 6, 21)
-#         )._checkpoint_filename
-#         expected = "DataIngestor.checkpoint"
-#         assert actual == expected
+# Subscrevendo um método de um determinada classe
+# Nesse caso estou informando que essa classe não tem mais nenhum objeto definido 
+@patch("ingestors.DataIngestor.__abstractmethod__", set())
+class TestIngestors:
+    def test_checkpoint_filename(self):
+        actual = DataIngestor(
+            writer=DataWriter,
+            coins=["TEST", "HOW"],
+            default_start_date=datetime.date(2021, 6, 21)
+        )._checkpoint_filename
+        expected = "DataIngestor.checkpoint"
+        assert actual == expected
